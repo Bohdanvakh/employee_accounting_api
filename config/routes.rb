a5f4991065b4c6a60e717a7c5c2a4225f41edc0d
Bohdan Vakh <bohdanvah@gmail.com>
2023-07-05 11:37:00 +0300

    fix: changed routes for vacation and position_history

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :departments
   resources :employees
-  resources :position_histories
+  # resources :position_histories
   resources :positions
+
+  resources :employees do
+    resources :position_histories, only: [:new, :create, :edit, :update]
+    resources :vacations, only: [:new, :create]
+  end
 end

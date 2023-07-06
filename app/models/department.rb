fb23a3e29dc6c1a007564f304d128bd80f8f2b0c
Bohdan Vakh <bohdanvah@gmail.com>
2023-07-06 18:21:44 +0300

    feat: added scope to current_manager method in department model

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,4 +3,12 @@ class Department < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 6, maximum: 120 }
   validates :abbreviation, presence: true, length: { minimum: 40, maximum: 400 }
+
+  scope :current_manager, -> {
+    joins(employees: { position_histories: :position })
+      .where(positions: { name: Position::MANAGER })
+      .where(position_histories: { finished_on: nil })
+      .order('position_histories.started_on DESC')
+      .limit(1)
+  }
 end

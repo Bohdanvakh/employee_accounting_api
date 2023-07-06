f30d983abc739587cd1bb02ac34235a8e54cfd13
Bohdan Vakh <bohdanvah@gmail.com>
2023-07-06 18:22:31 +0300

    refactor: changed routes to default format json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :departments
-  resources :employees
-  resources :position_histories
-  resources :positions
+  namespace :api do
+    namespace :v1 do
+      resources :departments, defaults: { format: :json }
+      resources :employees, defaults: { format: :json }
+      resources :positions, defaults: { format: :json }
+
+      resources :employees do
+        resources :position_histories, only: [:new, :create, :edit, :update], defaults: { format: :json }
+        resources :vacations, only: [:new, :create], defaults: { format: :json }
+      end
+    end
+  end
 end

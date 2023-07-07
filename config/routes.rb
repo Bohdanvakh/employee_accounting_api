59cc2229b9b60aa69c21887f22813f3dff40c515
Bohdan Vakh <bohdanvah@gmail.com>
2023-07-07 15:34:28 +0300

    fix: removed defaults in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :departments, defaults: { format: :json }
-      resources :employees, defaults: { format: :json }
-      resources :positions, defaults: { format: :json }
+      resources :departments
+      resources :employees
+      resources :positions
 
       resources :employees do
         resources :position_histories, only: [:new, :create, :edit, :update], defaults: { format: :json }
